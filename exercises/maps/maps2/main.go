61a81e6a6fb1b3c9c4db54771afedb6e072080d6
MBATheGamer <mbathegamer@gmail.com>
2024-04-26 19:55:53 +0100

    maps2 (Fixed)

--- exercises/maps/maps2/main.go
+++ exercises/maps/maps2/main.go
@@ -1,12 +1,16 @@
 // maps2
 // Make me compile!
-//
-// I AM NOT DONE
+
 package main
 
 import "fmt"
 
 func main() {
-	m := map{}
+	m := map[string]int{}
+
+
+	m["John"] = 25
+	m["Ana"] = 23
+
 	fmt.Printf("John is %d and Ana is %d", m["John"], m["Ana"])
 }
