c7d6192e3c1ce91199a8086373e82473a8c79c9a
MBATheGamer <mbathegamer@gmail.com>
2024-05-07 23:46:15 +0100

    structs1 (Fixed)

--- exercises/structs/structs1/main.go
+++ exercises/structs/structs1/main.go
@@ -1,15 +1,20 @@
 // structs1
 // Make me compile!
-//
-// I AM NOT DONE
+
 package main
 
 import "fmt"
 
 type Person struct {
-
+	name string
+	age  int
 }
 
 func main() {
+	var person = Person{
+		name: "Mohamed",
+		age:  26,
+	}
+
 	fmt.Printf("Person %s and age %d", person.name, person.age)
 }
